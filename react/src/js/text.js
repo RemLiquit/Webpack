6bd85b4479449ac8b295ae304e7867855f026b44
RemLiquit <ceixpp@gmail.com>
2020-11-09 20:23:34 -0400

    Soporte de JSX(React)

---
+++ react/src/js/text.js
@@ -0,0 +1,6 @@
+const text = () => {
+  document.body.innerHTML =
+    "<p>dorime, dorime ameno ameno latire, latire latire dorime</p>";
+};
+
+export default text;
---
+++ react/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "./src/js/index.js"),
+  },
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "webpack-dev-server",
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
